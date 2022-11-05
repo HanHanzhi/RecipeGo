63cef393b2e63c0ff32da23085dfd26e149c9044
Hanzhi <Hanzhi100@outlook.com>
2022-11-05 13:09:35 -0400

    build:adding of logo which routes back to homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import Pages from "./pages/Pages";
 import Search from "./components/Search";
 import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiCookingPot } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiCookingPot />
+          <Logo to={"/"}>RecipeNow</Logo>
+        </Nav>
         <Search />
         <Pages />
       </BrowserRouter>
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text0decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
